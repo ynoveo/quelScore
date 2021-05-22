d156f46f6af6fc537ced44021a170715b1b91902
ynoveo <19432156+ynoveo@users.noreply.github.com>
2021-05-22 22:03:25 +0000

    Finalisation réinitilisaiton du mot de passe

--- QuickStartApplication/webapp/controller/InitPassword.controller.js
+++ QuickStartApplication/webapp/controller/InitPassword.controller.js
@@ -8,14 +8,15 @@ sap.ui.define([
 	"sap/ui/layout/form/SimpleForm",
 	"sap/m/GroupHeaderListItem",
 	"sap/m/MessageToast",
-	"sap/ui/model/Filter"
-], function (Controller, History, JSONModel, Dialog, Button, TextField, SimpleForm, GroupHeaderListItem, MessageToast, Filter) {
+    "sap/ui/model/Filter",
+    "sap/m/Input",
+	"sap/m/Label"
+], function (Controller, History, JSONModel, Dialog, Button, TextField, SimpleForm, GroupHeaderListItem, MessageToast, Filter, Input, Label) {
 	"use strict";
 
 	return Controller.extend("QuickStartApplication.controller.Pronostics", {
-
-		onInit: function() {
-//--------  
+        onUserMatched: function(oEvent){
+            
             var passkey = decodeURIComponent(oEvent.getParameter("arguments").initkey);
 			var oController = this;
 			var dialog = new Dialog({
@@ -38,7 +39,7 @@ sap.ui.define([
                         var sPass = encodeURIComponent(sap.ui.getCore().byId("passc").getValue());
                         var sPass2 = encodeURIComponent(sap.ui.getCore().byId("passc2").getValue());
 						var sPreURL = sap.ui.getCore().getModel("global").getProperty("/preURL");
-						var sUrl = sPreURL + "JSON_V2021.php?action=SECINIT&key=" + sLogin + "&pass=" + sPass + "&passb=" + sPass2;
+						var sUrl = sPreURL + "JSON_V2021.php?action=SECINIT&key=" + passkey + "&pass=" + sPass + "&passb=" + sPass2;
 						var otModel = new JSONModel();
 
 						if(sap.ui.getCore().getModel("global").getProperty("/mode") === "test") {
@@ -50,13 +51,16 @@ sap.ui.define([
 						//MessageToast.show("code retour = " + otModel.getProperty("/reponse/retcode"));
 						if(otModel.getProperty("/reponse/retcode") === "0") {
                             MessageToast.show("Mise à jour du mot de passe réussie");
+                            setTimeout(sap.ui.core.UIComponent.getRouterFor(oController).navTo("appHome"), 5000);        
+                                              
                             // ++ redirection vers l'accueil : https://www.quelscore.com
 	    					dialog.close();
 						} else {
                             // message d'erreur 
                             MessageToast.show(otModel.getProperty("/reponse/retmsg"));
+
                             // si message d'erreur avec apikey non valide 
-                             MessageToast.show("La clé de réactivation n'est pas valide, vous devez faire une nouvelle demande de réinitilisation");
+                            // MessageToast.show("La clé de réactivation n'est pas valide, vous devez faire une nouvelle demande de réinitilisation");
                          // ++ redirection vers l'accueil : https://www.quelscore.com
 						}
 					}
@@ -65,8 +69,8 @@ sap.ui.define([
 				new Button({
 					text: 'Annuler',
 					press: function () {
+                        sap.ui.core.UIComponent.getRouterFor(oController).navTo("appHome");
                         dialog.close();
-                         // ++ redirection vers l'accueil : https://www.quelscore.com
 					}
 				})
 			],
@@ -85,6 +89,12 @@ sap.ui.define([
 
 			dialog.open();
 		
+
+        },
+		onInit: function() {
+//--------  
+            sap.ui.core.UIComponent.getRouterFor(this).getRoute("initPassword").attachPatternMatched(this.onUserMatched, this);
+          
 //-------
 
         }
